52152d00d8fc6d38482938a4cf2eabec01a036e7
root <root@localhost.localdomain>
2019-09-17 10:22:32 -0400

    ok

--- bytecode/bytecode64.s
+++ bytecode/bytecode64.s
@@ -11,23 +11,16 @@ push r9
 push r10
 push r11
 push rax
-
-sub rsp, 0x10
+sub rsp, 0xC
 mov [rsp], DWORD 0x2E2E2E2E
-mov [rsp + 0x4], DWORD 0x574f4f44
-mov [rsp + 0x8], DWORD 0x592e2e2e
-mov [rsp + 0x10], DWORD 0x2e0a
-;db '....WOODY....', 0x0A
+mov [rsp + 0x4], DWORD 0x444F4F57
+mov [rsp + 0x8], DWORD 0x2E2E2E59
+mov [rsp + 0xC], DWORD 0x0A2E
 mov rax, 1
 mov rdi, 1
-mov rsi, rsp
+lea rsi, [rsp]
 mov rdx, 14
 syscall
-;mov rax, 60
-;mov rdi, 0
-;syscall
-
-
 pop rax
 pop r11
 pop r10
@@ -38,4 +31,6 @@ pop rcx
 pop rax
 mov rsp, rbp
 pop rbp
-
+;mov rax, 60
+;mov rdi, 0
+;syscall
