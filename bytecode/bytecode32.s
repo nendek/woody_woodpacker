54f968d588c72fc5e70534776c72eecc0aec7f8c
root <root@localhost.localdomain>
2019-09-17 08:07:07 -0400

    fix bytecode32 maybe

--- bytecode/bytecode32.s
+++ bytecode/bytecode32.s
@@ -1,15 +1,22 @@
 global _start
 _start:
+push ebp
+mov ebp, esp
+and esp, 0xFFFFFFF0
+pusha
 jmp short str
 
 write:
-pop ecx
-mov eax, 4
-mov ebx, 1
 mov edx, 14
+pop ecx
+mov ebx, 1
+mov eax, 4
 int 0x80
 mov eax, 1
 int 0x80
+popa
+mov esp, ebp
+pop ebp
 
 str:
 call write
--- bytecode/compile.sh
+++ bytecode/compile.sh
@@ -5,9 +5,13 @@ ld -s -m elf_i386 -o exe32 bytecode32.o
 ld -s -o exe64 bytecode64.o
 rm bytecode32.o
 rm bytecode64.o
-(for i in $(objdump -d exe64 |grep "^ " |cut -f2); do echo -n 'x'$i; done;) > bytecode64
-(for i in $(objdump -d exe32 |grep "^ " |cut -f2); do echo -n 'x'$i; done;) > bytecode32
-#rm exe32
-#rm exe64
+printf "\"" > bytecode64
+printf "\"" > bytecode32
+(for i in $(objdump -d exe64 |grep "^ " |cut -f2); do echo -n 'x'$i; done;) >> bytecode64
+(for i in $(objdump -d exe32 |grep "^ " |cut -f2); do echo -n 'x'$i; done;) >> bytecode32
+printf "\";\n" >> bytecode64
+printf "\";\n" >> bytecode32
+rm exe32
+rm exe64
 sed -i 's/x/\\x/g' bytecode64
 sed -i 's/x/\\x/g' bytecode32
